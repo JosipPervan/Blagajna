d1ae56291930207b51ac78a769187501395a8bec
Josip Pervan <56668841+JosipPervan@users.noreply.github.com>
2023-01-24 22:17:51 +0100

    Add files via upload
    Nisam bežao

---
+++ blagajna/src/main/java/ba/sum/fsre/blagajna/repositories/KonobarRepository.java
@@ -0,0 +1,11 @@
+package ba.sum.fsre.blagajna.repositories;
+
+import ba.sum.fsre.blagajna.model.Konobar;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+public interface KonobarRepository extends
+        JpaRepository<Konobar, Long> {
+    @Query("SELECT u FROM Konobar u WHERE u.email = ?1")
+    public Konobar findByEmail(String email);
+}
